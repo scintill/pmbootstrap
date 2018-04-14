d3ee7499aa2e489a1204668bf7d61b04dadbcb60
Joey Hewitt <joey@joeyhewitt.com>
2018-04-14 12:47:00 -0600

    firmwareload: log to system log

--- aports/main/postmarketos-base/firmwareload.sh
+++ aports/main/postmarketos-base/firmwareload.sh
@@ -2,23 +2,23 @@
 
 FIRMWARE_DIRS="/lib/firmware /lib/firmware/postmarketos"
 
-exec >>/var/log/firmwareload.log 2>&1
+(
+	if [ ! -e "/sys/$DEVPATH/loading" ]; then
+		echo "firmware loader misses sysfs directory"
+		exit 1
+	fi
 
-if [ ! -e "/sys/$DEVPATH/loading" ]; then
-    echo "firmware loader misses sysfs directory"
-    exit 1
-fi
+	for DIR in $FIRMWARE_DIRS; do
+		[ -e "$DIR/$FIRMWARE" ] || continue
+		echo "loading $DIR/$FIRMWARE"
+		echo 1 > "/sys/$DEVPATH/loading"
+		cat "$DIR/$FIRMWARE" > "/sys/$DEVPATH/data"
+		echo 0 > "/sys/$DEVPATH/loading"
+		exit 0
+	done
 
-for DIR in $FIRMWARE_DIRS; do
-    [ -e "$DIR/$FIRMWARE" ] || continue
-    echo "loading $DIR/$FIRMWARE"
-    echo 1 > "/sys/$DEVPATH/loading"
-    cat "$DIR/$FIRMWARE" > "/sys/$DEVPATH/data"
-    echo 0 > "/sys/$DEVPATH/loading"
-    exit 0
-done
-
-# shellcheck disable=SC2039
-echo -1 > "/sys/$DEVPATH/loading"
-echo "cannot find firmware file '$FIRMWARE'"
-exit 1
+	# shellcheck disable=SC2039
+	echo -1 > "/sys/$DEVPATH/loading"
+	echo "cannot find firmware file '$FIRMWARE'"
+	exit 1
+) 2>&1 | logger -t "firmwareload"
